9c73651cfa9df2ff4703052d7def39ecd7e53c17
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-20 12:48:24 -0300

    resolução do requisito 12

--- src/challenges.js
+++ src/challenges.js
@@ -8,7 +8,6 @@ function compareTrue(animal1, animal2) {
   }
 }
 
-
 // Desafio 2
 function calcArea(base, height) {
   let resultado = (base * height) / 2;
@@ -18,7 +17,7 @@ console.log(calcArea());
 
 // Desafio 3
 function splitSentence(palavras) {
-  resultado = palavras.split(' ');
+  let resultado = palavras.split(' ');
   return resultado;
 }
 console.log(splitSentence('go Trybe'));
@@ -29,7 +28,7 @@ function concatName(names) {
     let firstName = names[0];
     let lastName = names[names.length - 1];
     let resultado = lastName + ', ' + firstName;
-    return resultado
+    return resultado;
   }
 }
 console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
@@ -46,77 +45,74 @@ console.log(footballPoints(10, 15));
 function highestCount(parametro) {
   let maiorNum = parametro[0];
   let contRepetido = 0;
-  for (let index = 0; index < parametro.length; index += 1){
-    if(maiorNum < parametro[index]){
-        maiorNum = parametro[index];
+  for (let index = 0; index < parametro.length; index += 1) {
+    if (maiorNum < parametro[index]) {
+      maiorNum = parametro[index];
     }
     contRepetido = 0;
-    for (let index2 = 0; index2 < parametro.length; index2 += 1){
-        if (maiorNum === parametro[index2]){
-            contRepetido += 1;
-        }
+    for (let index2 = 0; index2 < parametro.length; index2 += 1) {
+      if (maiorNum === parametro[index2]) {
+        contRepetido += 1;
+      }
     }
-}
-return contRepetido;
+  }
+  return contRepetido;
 }
 console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
 
 // Desafio 7
 function catAndMouse(posMouse, posCat1, posCat2) {
   let distCat1 = posCat1 - posMouse;
-    if (distCat1 < 0){
-      distCat1 = distCat1 * -1;
-    }
+  if (distCat1 < 0) {
+    distCat1 = distCat1 * -1;
+  }
   let distCat2 = posCat2 - posMouse;
-    if (distCat2 < 0){
-      distCat2 = distCat2 * -1;
-    }
-  if (distCat1 < distCat2){
+  if (distCat2 < 0) {
+    distCat2 = distCat2 * -1;
+  }
+  if (distCat1 < distCat2) {
     return 'cat1';
-  } else if (distCat2 < distCat1){
+  } else if (distCat2 < distCat1) {
     return 'cat2';
-  } else if (distCat1 === distCat2){
+  } else if (distCat1 === distCat2) {
     return 'os gatos trombam e o rato foge';
   }
 }
-console.log(catAndMouse(3,1,7));
+console.log(catAndMouse(3, 1, 7));
 
 // Desafio 8
 function fizzBuzz(parametro) {
   let valor = [];
-  for (let index = 0; index < parametro.length; index += 1){
-    if(parametro[index] % 3 === 0 && parametro[index] % 5 === 0){
-      valor.push ('fizzBuzz');
-    } else if (parametro[index] % 3 === 0){
-      valor.push ('fizz');
-    } else if (parametro[index] % 5 === 0){
-      valor.push ('buzz');
+  for (let index = 0; index < parametro.length; index += 1) {
+    if (parametro[index] % 3 === 0 && parametro[index] % 5 === 0) {
+      valor.push('fizzBuzz');
+    } else if (parametro[index] % 3 === 0) {
+      valor.push('fizz');
+    } else if (parametro[index] % 5 === 0) {
+      valor.push('buzz');
     } else {
-      valor.push ('bug!');
-    };
-  };
+      valor.push('bug!');
+    }
+  }
   return valor;
 }
 console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9
-//implementar uma funçao que codifica e decodifica
-//criar estrutura para substituir vogal por números
-// criar estrutura para substituir números por vogais
 function encode(parametro) {
   let resultado1 = parametro.replaceAll('a', '1');
-  let resultado2 = resultado1.replaceAll('e','2');
-  let resultado3 = resultado2.replaceAll('i','3');
-  let resultado4 = resultado3.replaceAll('o','4');
-  let resultado5 = resultado4.replaceAll('u','5');
+  let resultado2 = resultado1.replaceAll('e', '2');
+  let resultado3 = resultado2.replaceAll('i', '3');
+  let resultado4 = resultado3.replaceAll('o', '4');
+  let resultado5 = resultado4.replaceAll('u', '5');
   return resultado5;
 }
 function decode(parametro) {
   let resultado1 = parametro.replaceAll('1', 'a');
-  let resultado2 = resultado1.replaceAll('2','e');
-  let resultado3 = resultado2.replaceAll('3','i');
-  let resultado4 = resultado3.replaceAll('4','o');
-  let resultado5 = resultado4.replaceAll('5','u');
+  let resultado2 = resultado1.replaceAll('2', 'e');
+  let resultado3 = resultado2.replaceAll('3', 'i');
+  let resultado4 = resultado3.replaceAll('4', 'o');
+  let resultado5 = resultado4.replaceAll('5', 'u');
   return resultado5;
 }
 console.log(decode('H4w 1r2 y45 t4d1y?'));
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,54 +1,23 @@
 // Desafio 11
-function generatePhoneNumber(telefone) {
-  if (telefone.length !== 11) {
-    return 'Array com tamanho incorreto.';
-  }
-  for (let index = 0; index < telefone.length; index += 1) {
-    if (telefone[index] < 0 || telefone[index] > 9) {
-      return 'não é possível gerar um número de telefone com esses valores';
-    }
-    contRepeticao = 0;
-    for (let index2 = 0; index2 < telefone.length; index += 1) {
-      if (telefone[index] === telefone[index2]) {
-        contRepeticao += 1;
-      }
-      if (contRepeticao > 3) {
-        return 'não é possível gerar um número de telefone com esses valores';
-      }
-    }
-    return ('(' + telefone[0] + telefone[1] + ') ' + telefone[2] + telefone[3] + telefone[4] + telefone[5] + telefone[6] + '-' + telefone[7] + telefone[8] + telefone[9] + telefone[10]);
-  }
+function generatePhoneNumber() {
+
+
 }
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
-// exercicio 11
-//implementar a função
-//criar estrutura para retornar núemro padornizado
-//criar estrutura para informar tamanho incorrto
-// crirar estrutura para informar que não é possível gerar um numero com os valores informados.
 
 // Desafio 12
-function triangleCheck(lineA, lineB, lineC) {
-let condition1 = false;
-  if (lineA < lineB + lineC && lineB < lineA + lineC && lineC < lineA + lineC){
-    condition1 = true;
-    console.log(lineA,lineB,lineC,condition1)
-};
-let condition2 = false;
-if (lineA > Math.abs(lineB - lineC) && lineB > Math.abs(lineA - lineC) && lineC > Math.abs(lineB - lineA)){
-    condition2 = true;
-    console.log(lineA,lineB,lineC,condition2)
-};
-let ehTriangulo = false;
-if (condition1 === true && condition2 === true){
-    ehTriangulo = true;
-    console.log(condition1,condition2,ehTriangulo)
+function triangleCheck(lineA, lineB, lineC){
+  if (lineA > lineB + lineC || lineA < Math.abs(lineB - lineC)) {
+    return false
+} else if (lineB > lineA + lineC || lineB < Math.abs(lineA - lineC)) {
+    return false
+} else if (lineC > lineB + lineA || lineC < Math.abs(lineB - lineA)) {
+    return false
+} else {
+return true
 }
 }
-console.log(triangleCheck());
-//implementar a função
-//criar estrutura para verificar se qualquer lado é menor que a soma dos outros 2 lados
-//criar estrutura para verificar se o valor Absoluto(mód) da dif é maior que os outros 2 lados.
-// retornar a função com booleano
+console.log(triangleCheck(10,14,8));
+
 
 // Desafio 13
 function hydrate() {
