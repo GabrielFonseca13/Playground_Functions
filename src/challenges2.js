a0974391976fa0c4bd66ea4b2e9b900ea8666c77
Gabriel Fonseca <gabriel.fonseca@live.com>
2022-06-20 13:40:36 -0300

    ajustes dos requisitos bonus

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,57 +1,59 @@
 // Desafio 11
 function generatePhoneNumber(numTelefone) {
-if (numTelefone.length != 11) {
-  return 'numTelefone com tamanho incorreto.';
-}
-let counter = 0;
+  if (numTelefone.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  let counter = 0;
 for (let index = 0; index < numTelefone.length; index = index +1 ) {
   for (let index2 = 0; index2 < numTelefone.length; index2 = index2 +1 ) {
       if (numTelefone[index] === numTelefone[index2]) {
-          counter += 1;         
-      } if (counter === 3) {
-          return 'não é possível gerar um número de telefone com esses valores';
-      } if (numTelefone[index] > 9 || numTelefone[index] < 0) {
-          return 'não é possível gerar um número de telefone com esses valores';
+        counter += 1;
       }
+      if (counter === 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+      if (numTelefone[index] > 9 || numTelefone[index] < 0) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+    counter = 0;
   }
-  counter = 0;
-}
-numTelefone.splice(0, 0, "(");
-numTelefone.splice(3, 0, ")")
-numTelefone.splice(4, 0, " ")
-numTelefone.splice(10, 0, "-");
-numTelefone = numTelefone.join('');
-return numTelefone;
+  numTelefone.splice(0, 0, '(');
+  numTelefone.splice(3, 0, ')');
+  numTelefone.splice(4, 0, ' ');
+  numTelefone.splice(10, 0, '-');
+  numTelefone = numTelefone.join('');
+  return numTelefone;
 }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   if (lineA > lineB + lineC || lineA < Math.abs(lineB - lineC)) {
-           return false
-   } if (lineB > lineA + lineC || lineB < Math.abs(lineA - lineC)) {
-           return false
-   } if (lineC > lineB + lineA || lineC < Math.abs(lineB - lineA)) {
-           return false
-   } else {
-   return true
- }
+    return false;
+  }
+  if (lineB > lineA + lineC || lineB < Math.abs(lineA - lineC)) {
+    return false;
+  }
+  if (lineC > lineB + lineA || lineC < Math.abs(lineB - lineA)) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 // Desafio 13
-function hydrate(parametro) {
-        
+function hydrate(param) {
   let takeOutNumbers = /\d+/g;
-  let result = parametro.match(takeOutNumbers);
-  soma = 0;
-
-  for (index = 0; index < result.length; index = index +1) {
-    soma = soma + parseInt(result[index])          
+  let resultado = param.match(takeOutNumbers);
+  sum = 0;
+  for (index = 0; index < resultado.length; index = index + 1) {
+    sum = sum + parseInt(resultado[index]);
   }
-  if (soma === 1) {
-    return soma + ' copo de água';
-} else {
-return soma + ' copos de água';  
-}
+  if (sum === 1) {
+    return sum + ' copo de água';
+  } else {
+    return sum + ' copos de água';
+  }
 }
 
 module.exports = {
@@ -59,4 +61,3 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
-}
